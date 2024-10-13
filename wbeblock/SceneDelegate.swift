b71881b89c3395656843ceed7847c4bcbb96e769
Sachin Govind <bssachingovind@gmail.com>
2024-10-13 17:50:19 +0530

    Initial Commit

---
+++ wbeblock/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  wbeblock
+//
+//  Created by Sachin Govind on 13/10/24.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
---
+++ wbeblockTests/wbeblockTests.swift
@@ -0,0 +1,17 @@
+//
+//  wbeblockTests.swift
+//  wbeblockTests
+//
+//  Created by Sachin Govind on 13/10/24.
+//
+
+import Testing
+@testable import wbeblock
+
+struct wbeblockTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
