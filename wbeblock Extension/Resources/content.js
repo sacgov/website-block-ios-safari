056a670f2b67c373e3d895a1a2e5b627bcffedfe
Sachin Govind <bssachingovind@gmail.com>
2024-10-18 21:48:27 +0530

    first commit

--- wbeblock Extension/Resources/content.js
+++ wbeblock Extension/Resources/content.js
@@ -5,3 +5,34 @@ browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
     console.log("Received request: ", request);
 });
+
+function getDate () {
+    const date = new Date();
+
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+
+    // This arrangement can be altered based on how we want the date's format to appear.
+    return `${day}-${month}-${year}`;
+}
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+const keywords = ["swiggy","lichess"];
+const url = window.location.href;
+let keywordMatched = "";
+
+for(let i =0;i<keywords.length;i++){
+    if(url.includes(keywords[i])) {
+        keywordMatched = keywords[i];
+    }
+}
+
+keywordMatched = capitalizeFirstLetter(keywordMatched)
+
+
+
+document.body.innerHTML = `<h1 style="text-align:center"> <br><br> <br>Blocked  <p style="color:red;text-align:center">${keywordMatched}</p></h1>`
